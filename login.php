04abd962ef3ecb7c222cf7ed3434541fa16684d0
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2019-04-03 12:30:51 -0500

    Finished Login, logout backend and admin role. Working.

--- login.php
+++ login.php
@@ -4,17 +4,16 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Toy Store</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Mitr" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/site.css">
-    <script src="js/index.js"></script>
-    <script src="js/login.js"></script>
-  </head>
-  <body>
-    <?php include 'header.php' ?>
-<div class='content'>
+  <title>Toy Store</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Mitr" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/site.css">
+  <script src="js/login.js"></script>
+</head>
+<body>
+  <?php include 'header.php' ?>
+  <div class='content'>
     <main>
     <div class="container">
       <div class="login">
@@ -23,19 +22,19 @@ session_start();
         <div class="loginSmall" style="display: none;">If you are looking for the ToyWiz Buy List, please click <a href="https://tools.toywiz.com/buylist/">here</a>.<br/>&nbsp;</div>
 
             <div class="login-row">
-              <form class="login-form form" action="/login.php?action=check_login" method="post">
+              <form class="login-form form" action="session.php" method="post">
                   <div class="form-field">
-                      <label class="form-label" for="login_email">Email Address:</label>
-                      <input class="form-input" name="login_email" id="login_email" type="email">
+                      <label class="form-label" for="email">Email:</label>
+                      <input class="form-input" name="email" id="email" type="text">
                       <div id="emailInfo"></div>
                   </div>
                   <div class="form-field">
-                      <label class="form-label" for="login_pass">Password:</label>
-                      <input class="form-input" id="login_pass" type="password" name="login_pass">
+                      <label class="form-label" for="password">Password:</label>
+                      <input class="form-input" id="password" type="password" name="password">
                       <div id="passInfo"></div>
                   </div>
                   <div class="form-actions">
-                      <input type="submit" class="button button--primary" value="Login"><br/>
+                      <input type="submit" class="btn btn-primary login" value="Login"><br/>
                       <a class="forgot-password" href="/login.php?action=reset_password">Forgot your password?</a>
                   </div>
               </form>
@@ -48,10 +47,9 @@ session_start();
                           <p class="new-customer-intro">Create an account with us and you&#x27;ll be able to:</p>
                           <ul class="new-customer-fact-list">
                               <li class="new-customer-fact">Check out faster</li>
-                              <li class="new-customer-fact">Save multiple shipping addresses</li>
+                              <li class="new-customer-fact">Save shipping addresses</li>
                               <li class="new-customer-fact">Access your order history</li>
                               <li class="new-customer-fact">Track new orders</li>
-                              <li class="new-customer-fact">Save items to your wish list</li>
                           </ul>
                           <center><a href="register.php?action=create_account"><button class="button button--primary">Create Account</button></a></center>
                       </div>
@@ -61,12 +59,12 @@ session_start();
         </div>
       </div>
     </main>
-        </div>
+  </div>
   <?php include 'footer.html' ?>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ session.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include 'dbconfig.php';
+
+//Connect to mysql server
+$conn = mysqli_connect("$serverName:$port",$username,$password);
+
+if(mysqli_connect_errno()){
+  die("Databse connection failed".mysqli_connect_error());
+}
+//Select database
+mysqli_select_db($conn,$databaseName);
+//Construct the query
+$hashPassword = hash('sha256',$_POST['password']);
+$query = "SELECT * FROM User INNER JOIN roleUser
+ON User.userId = roleUser.userId
+INNER JOIN role
+ON role.roleId = roleUser.roleId WHERE email = '".$_POST['email']."' AND password = '$hashPassword'";
+
+//connect to db
+$result = mysqli_query($conn,$query);
+$user = mysqli_fetch_assoc($result);
+
+if($user == false){
+  header("Location: login.php");
+  die();
+}else{
+  //if user exist, set session username and email
+  $_SESSION['userName'] = $user['username'];
+  $_SESSION['email'] = $user['email'];
+  $_SESSION["isAdmin"] = $user['roleId'] == 1 ? true : false;
+  echo $user['roleId'];
+  echo (string)$_SESSION["isAdmin"] ;
+  header("Location: index.php");
+  die();
+}
+
+?>
