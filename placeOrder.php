159261b157453e687e7e3fbe739894919c7ac258
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2019-04-15 01:00:12 -0500

    Added query to insert to Sale and SaleProduct.

--- placeOrder.php
+++ placeOrder.php
@@ -1,25 +1,46 @@
+
+
 <?php
-    session_start();
-    include 'dbconfig.php';
-   $conn = mysqli_connect("$serverName:$port",$username,$password);
+session_start();
+include 'dbconfig.php';
 
-   if(mysqli_connect_errno()){
-       die("Database connection failed".mysqli_connect_error());
-   }
+$conn = mysqli_connect("$serverName:$port", $username, $password, $databaseName);
 
-   mysqli_select_db($conn,$databaseName);
+if (mysqli_connect_errno())
+    {
+    die("Database connection failed" . mysqli_connect_error());
+    }
 
-   $cartItems=$_SESSION['cartItems'];
-   foreach($cartItems as $item){
-     //echo $item->ID;
-     $queryString  = "UPDATE product SET quantity = quantity - ".$item->qty." WHERE product.ID = '".$item->ID."'";
-     //echo $queryString;
-     $result = mysqli_query($conn, $queryString);
-      unset($_SESSION['cartItems']);
+$cartItems = $_SESSION['cartItems'];
 
-      header("Location: index.php");
+// insert into Sale, SaleProduct, and Product (update quantity)
 
+$total = 0;
 
-      //insert changes to Sales and Salesorder table.
-   }
+foreach($cartItems as $item)
+    {
+    $total+= $item->qty * $item->Price;
+    }
+
+$queryString = "INSERT INTO Sale VALUES( NULL,'" . $_SESSION['userId'] . "','" . $total . "','1' )";
+$insertSales = mysqli_query($conn, $queryString);
+$currentSaleId =  mysqli_insert_id($conn);
+echo $currentSaleId;
+
+//reduce amount and save sales in database
+foreach($cartItems as $item)
+    {
+    $queryString = "UPDATE product SET quantity = quantity - " . $item->qty . " WHERE product.ID = '" . $item->ID . "'";
+
+    
+    $result = mysqli_query($conn, $queryString);
+    $queryString = "INSERT INTO SaleProduct VALUES ('" . $currentSaleId . "','" . $item->ID . "','".$item->qty."')";
+    echo $queryString;
+    $insertSaleProduct = mysqli_query($conn, $queryString);
+  }
+
+unset($_SESSION['cartItems']);
+mysqli_close($conn);
+header("Location: index.php");
 ?>
+
